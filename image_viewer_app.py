a4cfc1926639c144632ff63ff2e009201e410440
Jonbosss <jonathanfarhat@gmail.com>
2025-11-04 12:57:36 -0800

    added compatibility with 5 images with scrolling buttons that disable at end and beginning of list

--- image_viewer_app.py
+++ image_viewer_app.py
@@ -7,12 +7,69 @@ root.title("Image Viewer")
 root.iconbitmap(
     "c:/Users/jonat/Documents/Coding Practice/image_viewer/Icon.ico")
 
-my_img = ImageTk.PhotoImage(Image.open("Beginnings.png"))
-my_label = Label(image=my_img)
-my_label.pack()
+my_img1 = ImageTk.PhotoImage(Image.open("Beginnings.png"))
+my_img2 = ImageTk.PhotoImage(Image.open("Zoro.png"))
+my_img3 = ImageTk.PhotoImage(Image.open("Nami.png"))
+my_img4 = ImageTk.PhotoImage(Image.open("Sanji.png"))
+my_img5 = ImageTk.PhotoImage(Image.open("Usopp.png"))
 
 
-button_quit = Button(root, text="Exit Program", command=root.quit)
-button_quit.pack()
+image_list = [my_img1, my_img2, my_img3, my_img4, my_img5]
+
+
+my_label = Label(image=my_img1)
+my_label.grid(row=0, column=0, columnspan=3)
+
+# Button Functions
+
+
+def forward(image_number):
+    global my_label
+    global button_forward
+    global button_back
+
+    my_label.grid_forget()
+    my_label = Label(image=image_list[image_number-1])
+    button_forward = Button(
+        root, text=">>", command=lambda: forward(image_number+1))
+    button_back = Button(root, text="<<", command=lambda: back(image_number-1))
+
+    if image_number == 5:
+        button_forward = Button(root, text=">>", state=DISABLED)
+
+    my_label.grid(row=0, column=0, columnspan=3)
+    button_forward.grid(row=1, column=2)
+    button_back.grid(row=1, column=0)
+
+
+def back(image_number):
+    global my_label
+    global button_forward
+    global button_back
+
+    my_label.grid_forget()
+    my_label = Label(image=image_list[image_number-1])
+    button_forward = Button(
+        root, text=">>", command=lambda: forward(image_number+1))
+    button_back = Button(root, text="<<", command=lambda: back(image_number-1))
+
+    if image_number == 1:
+        button_back = Button(root, text="<<", state=DISABLED)
+
+    my_label.grid(row=0, column=0, columnspan=3)
+    button_forward.grid(row=1, column=2)
+    button_back.grid(row=1, column=0)
+
+# Buttons
+
+
+button_back = Button(root, text="<<", command=back, state=DISABLED)
+button_exit = Button(root, text="Exit Program", command=root.quit)
+button_forward = Button(root, text=">>", command=lambda: forward(2))
+
+button_back.grid(row=1, column=0)
+button_exit.grid(row=1, column=1)
+button_forward.grid(row=1, column=2)
+
 
 root.mainloop()
